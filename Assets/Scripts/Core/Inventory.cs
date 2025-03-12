d76518e102414d98fc93686f2a387cc3e1b985f6
Ivan Kosov <kosov.vanya@gmail.com>
2025-03-12 14:18:31 -0500

    Started working on inventory UI

--- Assets/Scripts/Core/Inventory.cs
+++ Assets/Scripts/Core/Inventory.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Core
 {
     class Inventory
     {
-        private const int invWidth = 10;
-        private const int invHeight = 16;
+        public const int invWidth = 10;
+        public const int invHeight = 16;
+        private readonly static Dictionary<ItemType, int> stackSizes = new()
+        {
+            { ItemType.Empty, 0 },
+            { ItemType.Wood, 100 },
+            { ItemType.Coal, 100 },
+            { ItemType.Iron_ore, 100 },
+            { ItemType.Copper_ore, 100 }
+        };
         private readonly Slot[,] inventory = new Slot[invWidth, invHeight];
+
+        public Inventory()
+        {
+
+        }
+
+        //public bool AddItem(Vector2Int slotPos, ItemType type, int amount)
+        //{
+        //    Slot slot = inventory[slotPos.x, slotPos.y];
+        //    if (slot.Type != type) return false;
+        //    if (slot.Amount )
+        //}
 
         private class Slot
         {
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,14 +1,41 @@
+using Assets.Scripts.Core;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    private readonly Inventory inventory = new();
+    private const float spaceBetweenSlots = 10f;
+    private float slotSize;
+    private bool invState = false;
+
+    public GameObject InventoryParent;
+    public Canvas Canvas;
+    public GameObject InventoryBackPrefab;
+    public GameObject SlotPrefab;
+
     void Start()
     {
-        
+        slotSize = SlotPrefab.GetComponent<RectTransform>().rect.width;
+
+        GenerateInventory();
+        InventoryParent.SetActive(invState);
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            invState = !invState;
+            InventoryParent.SetActive(invState);
+        }
+    }
+
+    private void GenerateInventory()
+    {
+        GameObject back = Instantiate(InventoryBackPrefab, InventoryParent.transform);
+        RectTransform rectTransform = back.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new(
+            slotSize * Inventory.invWidth + spaceBetweenSlots * (Inventory.invWidth + 1),
+            slotSize * Inventory.invHeight + spaceBetweenSlots * (Inventory.invHeight + 1));
     }
 }
