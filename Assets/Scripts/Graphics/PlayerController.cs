0de071261077bcfd0b3356c0d32ae5e104289ab2
Ivan Kosov <kosov.vanya@gmail.com>
2025-03-27 19:58:39 -0500

    Added player idle and running animations

--- Assets/Scripts/Graphics/PlayerController.cs
+++ Assets/Scripts/Graphics/PlayerController.cs
@@ -5,6 +5,9 @@ namespace Dev.Kosov.Factory.Graphics
 {
     public class PlayerController : MonoBehaviour
     {
+        private Vector2 prevMoveVector = new(0, 1);
+
+        public Animator MovementAnimator;
         public Camera Camera;
         public Rigidbody2D RigidBody;
         public float MoveSpeed;
@@ -35,6 +38,23 @@ namespace Dev.Kosov.Factory.Graphics
 
             Vector2 moveVector = new Vector2(xVel, yVel).normalized;
             RigidBody.MovePosition(RigidBody.position + Time.deltaTime * MoveSpeed * moveVector);
+
+            UpdateMovementAnimation(moveVector);
+        }
+
+        private void UpdateMovementAnimation(Vector2 moveVector)
+        {
+            if (moveVector.magnitude == 0) // Might have problems with precision
+            {
+                MovementAnimator.SetFloat("xOffset", prevMoveVector.x);
+                MovementAnimator.SetFloat("yOffset", prevMoveVector.y);
+
+                return;
+            }
+            prevMoveVector = moveVector;
+
+            MovementAnimator.SetFloat("xOffset", prevMoveVector.x * 2);
+            MovementAnimator.SetFloat("yOffset", prevMoveVector.y * 2);
         }
 
         private void Zoom()
