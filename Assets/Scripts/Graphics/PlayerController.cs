7a83ab096cd674a6bcf55f25a809e0c0dc16f303
Ivan Kosov <kosov.vanya@gmail.com>
2025-03-29 14:42:20 -0500

    Added shadow to idle and running player

--- Assets/Scripts/Graphics/PlayerController.cs
+++ Assets/Scripts/Graphics/PlayerController.cs
@@ -8,6 +8,7 @@ namespace Dev.Kosov.Factory.Graphics
         private Vector2 prevMoveVector = new(0, 1);
 
         public Animator MovementAnimator;
+        public Animator ShadowAnimator;
         public Camera Camera;
         public Rigidbody2D RigidBody;
         public float MoveSpeed;
@@ -39,15 +40,18 @@ namespace Dev.Kosov.Factory.Graphics
             Vector2 moveVector = new Vector2(xVel, yVel).normalized;
             RigidBody.MovePosition(RigidBody.position + Time.deltaTime * MoveSpeed * moveVector);
 
-            UpdateMovementAnimation(moveVector);
+            UpdateAnimations(moveVector);
         }
 
-        private void UpdateMovementAnimation(Vector2 moveVector)
+        private void UpdateAnimations(Vector2 moveVector)
         {
             if (moveVector.magnitude == 0) // Might have problems with precision
             {
                 MovementAnimator.SetFloat("xOffset", prevMoveVector.x);
                 MovementAnimator.SetFloat("yOffset", prevMoveVector.y);
+
+                ShadowAnimator.SetFloat("xOffset", prevMoveVector.x);
+                ShadowAnimator.SetFloat("yOffset", prevMoveVector.y);
 
                 return;
             }
@@ -55,6 +59,9 @@ namespace Dev.Kosov.Factory.Graphics
 
             MovementAnimator.SetFloat("xOffset", prevMoveVector.x * 2);
             MovementAnimator.SetFloat("yOffset", prevMoveVector.y * 2);
+
+            ShadowAnimator.SetFloat("xOffset", prevMoveVector.x * 2);
+            ShadowAnimator.SetFloat("yOffset", prevMoveVector.y * 2);
         }
 
         private void Zoom()
